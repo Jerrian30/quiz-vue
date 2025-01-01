db0ca03d9dbe204996f104684c3aab47f093f9aa
Jerrian <Jerrianlokal30@gmail.com>
2025-01-02 04:24:10 +0700

    component and props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-import {ref} from "vue"
+import {ref, watch} from "vue"
 import srcQuiz from "./data/quiz.json";
 
+import Card from "./components/QuizCard.vue";
+
 const quiz = ref(srcQuiz)
-console.log(quiz)
+const search = ref("")
+
+watch(search, () => {
+  quiz.value = srcQuiz.filter((quiz) => {
+    return quiz.title.toLowerCase().includes(search.value.toLowerCase())
+  });
+});
+
 </script>
 
 <template>
@@ -11,18 +20,10 @@ console.log(quiz)
     <div class="container max-w-[900px] mx-auto flex flex-col gap-10 mt-10 px-4">
       <header class="flex gap-3">
         <h1 id="title" class="font-bold">QUIZ VUE</h1>
-        <input type="text" name="" id="search-input" class="border text-md border-gray-500 rounded-md bg-gray-300">
+        <input v-model.trim="search" type="text" name="" id="search-input" class="border text-md border-gray-500 rounded-md bg-gray-300">
       </header>
       <section class="grid grid-cols-3 ">
-        <div class="border rounded-xl shadow-sm m-2" v-for="item in quiz" :key="item.id">
-          <div class="flex flex-col">
-            <img class="size-full rounded-t-xl" :src="item.img" alt="">
-            <div class="flex flex-col gap-2 m-2 mt">
-              <h5 class="text-md font-bold overflow-hidden">{{ item.title }}</h5>
-              <p class="text-sm overflow-hidden mb-1">2 questions</p>
-            </div>
-          </div>
-        </div>
+        <Card v-for="item in quiz" :key="item.id" :quiz="item" />
       </section>
     </div>
   </main>
---
+++ src/components/QuizCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { quiz } =defineProps(["quiz"]);
+
+</script>
+
+<template>
+     <div class="border rounded-xl shadow-sm m-2">
+          <div class="flex flex-col">
+            <img class="size-full rounded-t-xl" :src="quiz.img" alt="">
+            <div class="flex flex-col gap-2 m-2 mt">
+              <h5 class="text-md font-bold overflow-hidden">{{ quiz.title }}</h5>
+              <p class="text-sm overflow-hidden mb-1">{{ quiz.questions.length }} questions</p>
+            </div>
+          </div>
+        </div>
+</template>
